26ff84140d377910c25ecc9505ea1c0ab0bbc1cb
Anddroiden <32633250+Anddroiden@users.noreply.github.com>
2017-12-04 12:33:25 +0100

    Add files via upload

---
+++ MenuTest/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuTest
+{
+	class Menu
+	{
+		public static void Menustart()
+		{
+			Console.WriteLine("Vælg \n" +
+				"1. Udlån\n" +
+				"2. Udlånt\n" +
+				"3. Hjemme\n" +
+				"4. Aflever" +
+				"\n"+
+				"\n"+
+				"\n"+
+				"\n"+
+				"\n"+
+				"9. Afslut");
+
+			string input = Console.ReadLine();
+
+			switch(input){
+				case "1":
+					HomeMenu();
+					break;
+				case "2":
+					break;
+
+
+
+			}
+
+
+
+		}
+
+		public static void HomeMenu()
+		{
+			Console.Clear();
+			Console.WriteLine("du kom");
+			Console.ReadLine();
+		}
+	}
+}
